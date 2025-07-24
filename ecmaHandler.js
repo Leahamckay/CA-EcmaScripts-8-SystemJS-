32014f047dc2a1cb02a2bfb75b1f4765ee3a3685
AD:BB-Bishop <100320237+Leahamckay@users.noreply.github.com>
2025-07-24 16:43:15 -0500

    Create ecmaHandler.js
    Ecma Handler File to integrate in Github Actions Workflow for Python taks using Conda.

---
+++ ecmaHandler.js
@@ -0,0 +1,17 @@
+[![Python Package using Conda](https://github.com/Leahamckay/CA-EcmaScripts-8-SystemJS-/actions/workflows/python-package-conda.yml/badge.svg?branch=Leahamckay-patch-1&event=check_suite)](https://github.com/Leahamckay/CA-EcmaScripts-8-SystemJS-/actions/workflows/python-package-conda.yml)
+          module.exports = async function handleTask(eventData) {
+  console.log("Handling task triggered by GitHub Action");
+  // Example logic
+  if (eventData.type === "python-task") {
+    console.log("Python task detected");
+    // Custom logic here
+  }
+   module.exports = async function handleTask(eventData) {
+  console.log("Handling task triggered by GitHub Action");
+  // Example logic
+  if (eventData.type === "python-task") {
+    console.log("Python task detected");
+    // Custom logic here
+  }
+};         
+};                     
